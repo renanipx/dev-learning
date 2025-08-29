fe68479672a9d862d9d84459a35bc5ad21b21105
renanipx <137443957+renanipx@users.noreply.github.com>
2025-08-29 19:01:19 -0300

    feat: add number-to-word conversion function

---
+++ codewars/switchItUp.js
@@ -0,0 +1,21 @@
+// When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+
+// Input: 1
+// Output: "One".
+
+function switchItUp(number){
+  let dictionaryNumbers = {
+    '0': 'Zero',
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine',            
+  }
+  
+  return dictionaryNumbers[number]
+}
