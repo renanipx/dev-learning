21245bd0fe725898731f1d58548c6bbd85cfbd7d
renanipx <137443957+renanipx@users.noreply.github.com>
2025-08-29 22:42:01 -0300

    feat: remove first and last character from a string

---
+++ codewars/removeFirstLastCharacter.js
@@ -0,0 +1,19 @@
+// Remove First and Last Character
+
+// Your goal is to write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+// Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
+
+// Examples
+// 'eloquent' --> 'loquen'
+// 'country'  --> 'ountr' 
+// 'person'   --> 'erso'
+// 'ab'       --> '' (empty string)
+// 'xyz'      --> 'y'
+
+function removeChar(str) {
+    return str.slice(1, -1);
+};
+
+
+
+
