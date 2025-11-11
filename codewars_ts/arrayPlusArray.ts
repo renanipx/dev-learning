46eb6ced8ba27d8953b84aca084f7b8f9bfda5d0
renanipx <137443957+renanipx@users.noreply.github.com>
2025-11-10 21:58:05 -0300

    feat: add function to sum all elements from two arrays

---
+++ codewars_ts/arrayPlusArray.ts
@@ -0,0 +1,7 @@
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+export const arrayPlusArray = (arr1 : number[], arr2 : number[]) : number => {
+  return [...arr1, ...arr2].reduce((sum, value) => sum + value, 0)
+}
