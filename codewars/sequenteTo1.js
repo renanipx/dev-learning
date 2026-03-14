4f1e9d41ed2bd4c0ca365d4f0549186a6ceceaaf
renanipx <137443957+renanipx@users.noreply.github.com>
2026-03-13 22:10:30 -0300

    feat: implement seqToOne function to return a sequence from n to 1

---
+++ codewars/sequenteTo1.js
@@ -0,0 +1,28 @@
+// Get the number n to return the sequence from n to 1. The number n can be negative and also large number. (See the range as the following)
+
+// Example : 
+// n=5  >> [5,4,3,2,1]
+// n=-1 >> [-1,0,1]
+
+// Range :
+// Python     -9999 < n < 9999
+// Javascript -9999 < n < 9999
+// c++        -9999 < n < 9999
+// Crystal    -9999 < n < 9999
+// Ruby       -9999 < n < 9999
+
+function seqToOne(n) {
+  const result = [];
+
+  if (n > 0) {
+    for (let i = n; i >= 1; i--) {
+      result.push(i);
+    }
+  } else {
+    for (let i = n; i <= 1; i++) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
