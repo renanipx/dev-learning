798246809251d4eb9e8d9826b9b7483fbc5766d8
renanipx <137443957+renanipx@users.noreply.github.com>
2026-03-29 22:21:12 -0300

    feat: implement check function to verify if an array contains a specified value

---
+++ codewars_ts/youOnlyNeedOneBeginner.ts
@@ -0,0 +1,9 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// a can contain numbers or strings. x can be either.
+
+// Return true if the array contains the value, false if not.
+
+export const check = (a: (number | string)[], x: number | string): boolean => {
+  return a.includes(x);
+}
